5dc9f4703d551a16417f3cd60f2b59c0106c56fd
OmArCaMc <omar.camacho@ucr.ac.cr>
2024-10-26 12:30:20 -0600

    especificacion de instrucciones quemadas

--- app/src/main/java/com/example/animate/topic1screen.java
+++ app/src/main/java/com/example/animate/topic1screen.java
@@ -56,8 +56,8 @@ public class topic1screen extends AppCompatActivity {
                 intent.putExtra("FRAME1", R.drawable.generic_egg1_asset);
                 intent.putExtra("DRAGGABLE2", R.drawable.generic_squirrel_asset);
                 intent.putExtra("FRAME2", R.drawable.generic_tree_asset_1);
-                intent.putExtra("INSTRUCTION", R.string.Instruction_1_1);
-                intent.putExtra("BACK_BUTTON_IMAGE", R.drawable.back_topic1_4_asset);
+                intent.putExtra("INSTRUCTION", "Relaciona los animales (quemada,TODO:arreglar)");
+                intent.putExtra("BACK_BUTTON_IMAGE", R.drawable.back_topic1_1_asset);
 
                 startActivity(intent);
             }
@@ -71,8 +71,8 @@ public class topic1screen extends AppCompatActivity {
 
                 intent.putExtra("DRAGGABLE0", R.drawable.generic_monkey_1_asset);
                 intent.putExtra("FIXED0", R.drawable.generic_tree_asset_1);
-                intent.putExtra("INSTRUCTION", R.string.Instruction_1_2);
-                intent.putExtra("BACK_BUTTON_IMAGE", R.drawable.back_topic1_4_asset);
+                intent.putExtra("INSTRUCTION", "Coloca el mono en la base del árbol (quemada,TODO:arreglar)");
+                intent.putExtra("BACK_BUTTON_IMAGE", R.drawable.back_topic1_2_asset);
                 // above, below, left, right
                 intent.putExtra("EXPECTED_POSITION", "below");
                 startActivity(intent);
