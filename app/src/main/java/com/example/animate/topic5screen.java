1b5fd92d69499cc3281e8f2c9155fca2d265806d
Estefania Montero <estefania.monteronavarro@ucr.ac.cr>
2024-11-19 07:55:01 -0600

    Corrections regarding topic screen

--- app/src/main/java/com/example/animate/topic5screen.java
+++ app/src/main/java/com/example/animate/topic5screen.java
@@ -1,5 +1,6 @@
 package com.example.animate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,12 @@ import androidx.core.view.WindowInsetsCompat;
 public class topic5screen extends AppCompatActivity {
     // Declare a button named back to return.
     Button back;
+    Button question1;
+    Button question2;
+    Button question3;
+    Button question4;
+    Button question5;
+    Button question6;
 
     @Override
     // Method called when the activity is first created.
@@ -42,6 +49,108 @@ public class topic5screen extends AppCompatActivity {
             public void onClick(View v) {
                 // Finish the current activity and go back to the previous activity.
                 finish();
+            }
+        });
+
+        question1 = (Button) findViewById(R.id.question1);
+        question1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(topic5screen.this, dragMultipleImagesMod.class);
+                intent.putExtra("DRAGGABLE0", R.drawable.generic_banana_asset);
+                intent.putExtra("FRAME0", R.drawable.generic_monkey_2_asset);
+                intent.putExtra("DRAGGABLE1", R.drawable.generic_fish_asset);
+                intent.putExtra("FRAME1", R.drawable.generic_bear_asset);
+                intent.putExtra("DRAGGABLE2", R.drawable.generic_butterfly_asset);
+                intent.putExtra("FRAME2", R.drawable.generic_chamaleon_asset);
+                intent.putExtra("INSTRUCTION", R.string.Instruction_3_1);
+                intent.putExtra("BACK_BUTTON_IMAGE", R.drawable.back_topic5_1_asset);
+                startActivity(intent);
+            }
+        });
+
+        question2 = (Button) findViewById(R.id.question2);
+        question2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(topic5screen.this, SelectOptionMod.class);
+
+                intent.putExtra("INSTRUCTION", R.string.Instruction_3_2);
+                intent.putExtra("ILLUSTRATION", R.drawable.question_5_2_bg);
+                intent.putExtra("OP1", R.drawable.generic_cat_asset);
+                intent.putExtra("OP2", R.drawable.generic_hamster_asset);
+                intent.putExtra("OP3", R.drawable.generic_monkey_1_asset);
+                intent.putExtra("RightOp", 3); // Establece la opción correcta (ej. 1)
+                intent.putExtra("BACK_BUTTON_IMAGE", R.drawable.back_topic5_2_asset);
+                startActivity(intent);
+            }
+        });
+
+        question3 = (Button) findViewById(R.id.question3);
+        question3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(topic5screen.this, dragMultipleImagesMod.class);
+                intent.putExtra("DRAGGABLE0", R.drawable.generic_cat_asset);
+                intent.putExtra("FRAME0", R.drawable.generic_orange_cat_asset);
+                intent.putExtra("DRAGGABLE1", R.drawable.generic_monkey_1_asset);
+                intent.putExtra("FRAME1", R.drawable.generic_monkey_2_asset);
+                intent.putExtra("DRAGGABLE2", R.drawable.generic_bird_asset);
+                intent.putExtra("FRAME2", R.drawable.generic_tucan_asset);
+                intent.putExtra("INSTRUCTION", R.string.Instruction_3_3);
+                intent.putExtra("BACK_BUTTON_IMAGE", R.drawable.back_topic5_3_asset);
+                startActivity(intent);
+            }
+        });
+
+        question4 = (Button) findViewById(R.id.question4);
+        question4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(topic5screen.this, SelectOptionMod.class);
+
+                intent.putExtra("INSTRUCTION", R.string.Instruction_3_4);
+                intent.putExtra("ILLUSTRATION", R.drawable.question_5_4_bg);
+                intent.putExtra("OP1", R.drawable.generic_nest_asset);
+                intent.putExtra("OP2", R.drawable.question_4_5_static_1);
+                intent.putExtra("OP3", R.drawable.question_4_5_static_2);
+                intent.putExtra("RightOp", 2); // Establece la opción correcta (ej. 1)
+                intent.putExtra("BACK_BUTTON_IMAGE", R.drawable.back_topic5_4_asset);
+                startActivity(intent);
+            }
+        });
+
+        question5 = (Button) findViewById(R.id.question5);
+        question5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(topic5screen.this, dragMultipleImagesMod.class);
+                intent.putExtra("DRAGGABLE0", R.drawable.question_5_5_1_draggable);
+                intent.putExtra("FRAME0", R.drawable.question_5_5_1_static);
+                intent.putExtra("DRAGGABLE1", R.drawable.question_5_5_2_draggable);
+                intent.putExtra("FRAME1", R.drawable.question_5_5_2_static);
+                intent.putExtra("DRAGGABLE2", R.drawable.generic_banana_asset);
+                intent.putExtra("FRAME2", R.drawable.generic_monkey_1_asset);
+                intent.putExtra("INSTRUCTION", R.string.Instruction_3_5);
+                intent.putExtra("BACK_BUTTON_IMAGE", R.drawable.back_topic5_5_asset);
+                startActivity(intent);
+            }
+        });
+
+        question6 = (Button) findViewById(R.id.question6);
+        question6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(topic5screen.this, dragMultipleImagesMod.class);
+                intent.putExtra("DRAGGABLE0", R.drawable.generic_strawberry_asset);
+                intent.putExtra("FRAME0", R.drawable.generic_duck_asset);
+                intent.putExtra("DRAGGABLE1", R.drawable.question_5_6_draggable2);
+                intent.putExtra("FRAME1", R.drawable.generic_turtle_asset);
+                intent.putExtra("DRAGGABLE2", R.drawable.question_5_6_draggable_1);
+                intent.putExtra("FRAME2", R.drawable.generic_dog_asset);
+                intent.putExtra("INSTRUCTION", R.string.Instruction_3_6);
+                intent.putExtra("BACK_BUTTON_IMAGE", R.drawable.back_topic5_6_asset);
+                startActivity(intent);
             }
         });
     }
