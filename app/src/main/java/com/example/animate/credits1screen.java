8eae3951e3635c418ee2da3d01bbe392f1fff8f3
OmArCaMc <omar.camacho@ucr.ac.cr>
2024-12-07 21:37:12 -0600

    Las 3 pantallas de creditos agregadas

--- app/src/main/java/com/example/animate/credits1screen.java
+++ app/src/main/java/com/example/animate/credits1screen.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,37 +11,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class startActivity extends AppCompatActivity {
+public class credits1screen extends AppCompatActivity {
 
-    Button start;
-    Button credits;
+    Button menu;
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_credits1);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        start = (Button) findViewById(R.id.startBttn);
-        start.setOnClickListener(new View.OnClickListener() {
+        // Inicialización y configuración del botón
+        menu = findViewById(R.id.buttonMenu);
+        menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(startActivity.this, menu.class);
+                // Navegar de regreso a startActivity
+                Intent intent = new Intent(credits1screen.this, startActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
 
-        credits = (Button) findViewById(R.id.creditsBttn);
-        credits.setOnClickListener(new View.OnClickListener() {
+        next = findViewById(R.id.buttonNext);
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(startActivity.this, credits1screen.class);
+                Intent intent = new Intent(credits1screen.this, credits2screen.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/animate/credits2screen.java
+++ app/src/main/java/com/example/animate/credits2screen.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,37 +11,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class startActivity extends AppCompatActivity {
+public class credits2screen extends AppCompatActivity {
 
-    Button start;
-    Button credits;
+    Button menu;
+    Button next;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_credits2);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        start = (Button) findViewById(R.id.startBttn);
-        start.setOnClickListener(new View.OnClickListener() {
+        menu = findViewById(R.id.buttonMenu);
+        menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(startActivity.this, menu.class);
+                Intent intent = new Intent(credits2screen.this, startActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
 
-        credits = (Button) findViewById(R.id.creditsBttn);
-        credits.setOnClickListener(new View.OnClickListener() {
+        back = (Button) findViewById(R.id.buttonBack);
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(startActivity.this, credits1screen.class);
+                Intent intent = new Intent(credits2screen.this, credits1screen.class);
                 startActivity(intent);
+                finish();
+            }
+        });
+
+        next = findViewById(R.id.buttonNext);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(credits2screen.this, credits3screen.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
